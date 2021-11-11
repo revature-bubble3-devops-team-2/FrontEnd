1621bb28f280996e96f73dece812b80b218516b0
henry ramirez <you@example.com>
2021-11-11 14:01:00 -0800

    added authorization headers to register

--- src/app/views/register/register/register.component.ts
+++ src/app/views/register/register/register.component.ts
@@ -46,6 +46,9 @@ export class RegisterComponent implements OnInit {
           }
         )
       }
+      else{
+        this.pswMatch = true;
+      }
     } 
     else{
       this.missing = true;
