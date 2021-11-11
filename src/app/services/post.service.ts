744ffd29586f28501bdde586485ffc05b9ad384c
dawmckin <dawmckin20@gmail.com>
2021-11-11 18:11:47 -0500

    fixed merge conflicts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../models/post';
-<<<<<<< HEAD
 import { Observable } from 'rxjs';
-=======
 import { environment } from '../../environments/environment';
-
->>>>>>> df8e35ab204855995f7baee785cc4192af23c2b3
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +10,12 @@ import { environment } from '../../environments/environment';
 export class PostService {
 
   public createPost(post: Post) {
-<<<<<<< HEAD
     console.log(post);
     return this.httpClient.post<Post>("http://localhost:8082/posts", post);
-=======
     return this.httpClient.post<Post>(environment.postURL, post, { headers: {
       "Authorization" : `${sessionStorage.getItem('token')}`
     }});
 
->>>>>>> df8e35ab204855995f7baee785cc4192af23c2b3
   }
 
   public getAllPosts(): Observable<Post[]> {
