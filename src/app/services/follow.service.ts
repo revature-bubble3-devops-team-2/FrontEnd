7555e12213abd6410366a0e73dd696639fc672e7
kalebconner408 <kalebconner408@gmail.com>
2021-11-18 10:43:20 -0600

    Change auth token to a string literal

--- src/app/services/follow.service.ts
+++ src/app/services/follow.service.ts
@@ -12,57 +12,41 @@ export class FollowService {
 
   followUserByEmail(email: string): Observable<HttpResponse<string>>
   {
-    let auth = this.token;
-    if (auth == null)
-      auth = "";
-
     return this.http.post<string>
     (
       "http://localhost:8082/profile/follow",
       { "email":email },
-      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth}}
+      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': `${this.token}`}}
     );
   }
   
   followUserById(id: number): Observable<HttpResponse<string>>
   {
-    let auth = this.token;
-    if (auth == null)
-      auth = "";
-
     return this.http.post<string>
     (
       "http://localhost:8082/profile/follow",
       { "id":id },
-      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth}}
+      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': `${this.token}`}}
     );
   }
 
   unfollowUserByEmail(email: string): Observable<HttpResponse<string>>
   {
-    let auth = this.token;
-    if (auth == null)
-      auth = "";
-
     return this.http.post<string>
     (
       "http://localhost:8082/profile/unfollow",
       { "id":email },
-      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth}}
+      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': `${this.token}`}}
     )
   }
 
   unfollowUserById(id: number): Observable<HttpResponse<string>>
   {
-    let auth = this.token;
-    if (auth == null)
-      auth = "";
-
     return this.http.post<string>
     (
       "http://localhost:8082/profile/unfollow",
       { "id":id },
-      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth}}
+      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': `${this.token}`}}
     )
   }
 }
