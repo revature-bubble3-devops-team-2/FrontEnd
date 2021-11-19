57e5cbc59911cff0403d98eac3007df0d97251f3
Michael <mxledesma@gmail.com>
2021-11-19 16:22:54 -0600

    commit changes to module.ts,, profile.ts modal-button.component and environment.ts

--- src/app/views/login/login/login.component.ts
+++ src/app/views/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router} from '@angular/router';
 import { Profile } from '../../../models/profile';
 import { ProfileService } from '../../../services/profile.service';
 
