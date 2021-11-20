e7335d95c948b8c7e8b0a103c90b157f58c5ea5d
dhrubot <dhrubo.dh@gmail.com>
2021-11-20 06:11:29 -0500

    re added the post feed component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './views/login/login/login.component';
 import { RegisterComponent } from './views/register/register/register.component';
 import { HomeComponent } from './views/home/home/home.component';
 import { PostsContainerComponent } from './views/posts/posts-container/posts-container.component';
+import { PostFeedComponent } from './views/posts/post-feed/post-feed.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'home', component: HomeComponent},
   {path: 'posts', component: PostsContainerComponent},
+  {path: 'feed', component: PostFeedComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
 
