c0dc3f951391aab5fed0a2f0191f6e43bcd3d575
April Weaver <april.weaver@revature.net>
2021-11-22 10:32:25 -0500

    first push

--- src/app/views/home/home/home.component.ts
+++ src/app/views/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'app/models/post';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  post: Post = {
+    psid: 1,
+    creator: {
+      pid: 1,
+      username: 'profile3',
+      passkey: '33',
+      firstName: 'Three',
+      lastName: 'LastThree',
+      email: 'Email3',
+    },
+    body: 'Hello',
+    datePosted: new Date(),
+    imgURL: 'image_url1',
+  };
 
   constructor() { }
 
