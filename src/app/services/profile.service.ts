ae564e369956837ffe7b8afe6a04fc4e3c7c30d5
Sitilac <m.calitis@gmail.com>
2021-11-22 11:09:37 -0800

    Modified profile sizing

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse} from '@angular/common/http';
+import { HttpClient, HttpResponse,HttpHeaders} from '@angular/common/http';
 import { Profile } from '../models/profile';
 import { Observable } from 'rxjs';
 
