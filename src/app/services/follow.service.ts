b74a00cf75019114017738581fabb5bdbc7607ab
kalebconner408 <kalebconner408@gmail.com>
2021-11-22 17:35:38 -0600

    Fix some merge conflicts 6

--- src/app/services/follow.service.ts
+++ src/app/services/follow.service.ts
@@ -22,23 +22,29 @@ export class FollowService {
 
   unfollowUserByEmail(email: string): Observable<HttpResponse<string>>
   {
+    let auth = this.token;
+    if (auth == null)
+      auth = "";
+
     return this.http.post<string>
     (
-      
-      `http://localhost:8082/profile/unfollow?Authorization=${this.token}`,
+      "http://localhost:8082/profile/unfollow",
       { "id":email },
-      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth}}
     )
   }
 
   unfollowUserById(id: number): Observable<HttpResponse<string>>
   {
+    let auth = this.token;
+    if (auth == null)
+      auth = "";
+
     return this.http.post<string>
     (
-      
-      `http://localhost:8082/profile/unfollow?Authorization=${this.token}`,
+      "http://localhost:8082/profile/unfollow",
       { "id":id },
-      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+      { observe: 'response', headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': auth}}
     )
   }
 }
