5c11cc91ab6d6b02e9d5b2080d0abf54cea55eb2
April Weaver <april.weaver@revature.net>
2021-11-23 14:59:47 -0500

    sonar cloud bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login/login.component';
 import { RegisterComponent } from './views/register/register/register.component';
 import { HomeComponent } from './views/home/home/home.component';
-import { PostsContainerComponent } from './views/posts/posts-container/posts-container.component';
-import { PostFeedComponent } from './views/posts/post-feed/post-feed.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'posts', component: PostsContainerComponent},
-  {path: 'feed', component: PostFeedComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
 
