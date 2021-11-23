9c1041c410b70d0561a69b4b5fee8e76b8a33c47
April Weaver <april.weaver@revature.net>
2021-11-23 15:13:16 -0500

    forgot to add enviromental variables to comments

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Comment } from 'app/models/comment';
 import { Observable } from 'rxjs';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +13,16 @@ export class CommentService {
 
   createComment(comment: Comment): Observable<any> {
     console.log(comment);
-    return this.http.post('http://localhost:8082/comment', comment)
+    return this.http.post(`${environment.url}/comment`, comment)
   }
 
   getCommentsByPsid(psid: number): Observable<any> {
     console.log(psid);
-    return this.http.get(`http://localhost:8082/comment?psid=${psid}`)
+    return this.http.get(`${environment.url}/comment?psid=${psid}`)
   }
 
   deleteCommentByCid(cid: number): Observable<any> {
     console.log(cid);
-    return this.http.delete(`http://localhost:8082/comment/${cid}`)
+    return this.http.delete(`${environment.url}/comment/${cid}`)
   }
 }
