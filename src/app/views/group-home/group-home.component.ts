c95dc28b5ed580d47402cf8a4174c4978d6bcfb5
AracenaSC <aracenacristopher@gmail.com>
2022-02-03 14:33:23 -0500

    merging profile staging

---
+++ src/app/views/group-home/group-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-home',
+  templateUrl: './group-home.component.html',
+  styleUrls: ['./group-home.component.css']
+})
+export class GroupHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
