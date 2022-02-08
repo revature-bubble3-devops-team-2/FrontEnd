0d536009b1b319ac77c47febf23287a4e52e96e4
AracenaSC <aracenacristopher@gmail.com>
2022-02-07 20:20:18 -0500

    merging zak fully functional

--- src/app/views/group-home/group-home.component.ts
+++ src/app/views/group-home/group-home.component.ts
@@ -1,17 +1,19 @@
+import { ProfileService } from 'app/services/profile.service';
+import { Profile } from 'app/models/profile';
+import { GroupService } from 'app/services/group.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-group-home',
   templateUrl: './group-home.component.html',
-  styleUrls: ['./group-home.component.css']
+  styleUrls: ['./group-home.component.css'],
 })
 export class GroupHomeComponent implements OnInit {
+  constructor(
+    public groupService: GroupService,
+    public profileService: ProfileService,
+    public profile: Profile
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-
-
+  ngOnInit(): void {}
 }
