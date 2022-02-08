347187f1a78f5982fa963337b1f881f8ef320968
Aaron Medlock <A-Medlock@hotmail.com>
2022-02-08 14:49:44 -0800

    Merged appearance changes with main

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:5001',
-  angUrl: 'http://localhost:4200'
-  // url: 'http://p3211129app-env.eba-ekniyau3.us-east-1.elasticbeanstalk.com:5000',
-  // angUrl: 'http://revature-bubble-2.s3-website-us-east-1.amazonaws.com'
+  // url: 'http://localhost:5001',
+  // angUrl: 'http://localhost:4200'
+  url: 'http://p3211129app-env.eba-ekniyau3.us-east-1.elasticbeanstalk.com:5000',
+  angUrl: 'http://revature-bubble-2.s3-website-us-east-1.amazonaws.com'
 
 };
