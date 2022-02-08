e5771907196606bcc0041feef8d5503c88963ca0
Aaron Medlock <A-Medlock@hotmail.com>
2022-02-08 15:40:42 -0800

    Styled the email verification page

--- src/app/views/verify-email/verify-email.component.ts
+++ src/app/views/verify-email/verify-email.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProfileService } from 'app/services/profile.service';
+import { faLaughBeam } from '@fortawesome/free-solid-svg-icons';
+import { faFrown} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-verify-email',
@@ -12,31 +14,37 @@ export class VerifyEmailComponent implements OnInit {
    confirmed?: boolean;
    randomCode ?: string;
    email ?: string;
+
+   // Font Awesome Icons
+   faFrown = faFrown;
+   faLaughBeam = faLaughBeam;
+
+
   constructor(private profileService: ProfileService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.route.queryParams
-      .subscribe(params => {
-        console.log(params);
-        this.randomCode = params.randomCode;
-        this.email = params.email;
-        console.log(this.randomCode);
+    // this.route.queryParams
+    //   .subscribe(params => {
+    //     console.log(params);
+    //     this.randomCode = params.randomCode;
+    //     this.email = params.email;
+    //     console.log(this.randomCode);
 
-        if (this.randomCode == localStorage.getItem('randomCode')){
-          this.confirmed = true;
-          this.verifyUser(params.email);
-          localStorage.removeItem('randomCode');
-          setTimeout(() => {
-            this.router.navigate(['/login']);
-          }, 3000);
-        } else {
-          this.confirmed = false;
-          setTimeout(() => {
-            this.router.navigate(['/login']);
-          }, 3000);
-        }
+    //     if (this.randomCode == localStorage.getItem('randomCode')){
+    //       this.confirmed = true;
+    //       this.verifyUser(params.email);
+    //       localStorage.removeItem('randomCode');
+    //       setTimeout(() => {
+    //         this.router.navigate(['/login']);
+    //       }, 3000);
+    //     } else {
+    //       this.confirmed = false;
+    //       setTimeout(() => {
+    //         this.router.navigate(['/login']);
+    //       }, 3000);
+    //     }
 
-      });
+    //   });
   }
 
   verifyUser(mail: string): void {
