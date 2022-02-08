310d93b43777502a1d8f266cb190c89b4e5460ec
Kenny Phu <kphu@outlook.com>
2022-02-08 11:14:39 -0500

    update urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-
   url: 'http://p3211129app-env.eba-ekniyau3.us-east-1.elasticbeanstalk.com:5000',
   angUrl: 'http://revature-bubble-2.s3-website-us-east-1.amazonaws.com'
 
