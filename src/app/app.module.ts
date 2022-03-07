01434d7ab589fd6a834fbffdce22e56e632d66fa
Ryan Sy <ryanbsy@gmail.com>
2022-03-07 17:04:28 -0500

    got dark mode workling on login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { CheckEmailComponent } from './views/check-email/check-email.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { GroupHeaderComponent } from './views/group-header/group-header.component';
 import { CreateGroupPostComponent } from './views/create-group-post/create-group-post.component';
+import { DarkmodeToggleComponent } from './views/darkmode-toggle/darkmode-toggle.component';
 
 @NgModule({
   declarations: [
@@ -68,7 +69,8 @@ import { CreateGroupPostComponent } from './views/create-group-post/create-group
     VerifyEmailComponent,
     CheckEmailComponent,
     GroupHeaderComponent,
-    CreateGroupPostComponent
+    CreateGroupPostComponent,
+    DarkmodeToggleComponent
   ],
   imports: [
     BrowserModule,
