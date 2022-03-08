410dcb0e2eee80d7dd917a4a64f6787096bda9c8
Davidgin <davidgin641@gmail.com>
2022-03-08 11:02:13 -0800

    Signed-off-by: Davidgin <davidgin641@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://35.170.242.190:5000',
-  angUrl: 'http://revature-bubble-2.s3-website-us-east-1.amazonaws.com'
+  url: 'http://localhost:5000',
+  angUrl: 'http://localhost:4200/'
 
 };
