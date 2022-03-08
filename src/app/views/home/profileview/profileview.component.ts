db9c220e4eeb3bd99ecb24fb21f06e4d7fb230b3
amsvang <amyvangsinbox@outlook.com>
2022-03-08 12:30:33 -0800

    switch toggle update

--- src/app/views/home/profileview/profileview.component.ts
+++ src/app/views/home/profileview/profileview.component.ts
@@ -39,7 +39,8 @@ export class ProfileviewComponent implements OnInit {
   //Tabs
   showPosts: boolean = true;
   showFollowers: boolean = false;
-  showFollowing : boolean = false;
+  // showFollowing : boolean = false;
+  showGroups: boolean = false;
 
 
 
@@ -110,13 +111,20 @@ export class ProfileviewComponent implements OnInit {
 
 
 
-toggleViewTabs(){
-  if(this.showPosts){
-    this.showPosts = false;
-    this.showFollowers = true;
-  } else {
-    this.showPosts = true;
-    this.showFollowers = false;
+toggleViewTabs(index: number){
+  this.showPosts = false;
+  this.showFollowers = false;
+  this.showGroups = false;
+  switch(index){
+    case 0:
+      this.showPosts = true;
+      break;
+    case 1:
+      this.showFollowers = true;
+      break;
+    case 2:
+      this.showGroups = true;
+      break;
   }
 }
 
