b87c64608220fd877468703f6eb415e70ad3d494
germygrimes <germygrimes@gmail.com>
2022-03-08 17:24:09 -0500

    It's a mess right now. But, trying to create a 'add-group-cover-photo' component using 'create-post' component as a template because on messing around on website, the only place successfully can upload photo is when creating a post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { CheckEmailComponent } from './views/check-email/check-email.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { GroupHeaderComponent } from './views/group-header/group-header.component';
 import { CreateGroupPostComponent } from './views/create-group-post/create-group-post.component';
+//import { AddGroupCoverPhotoComponent } from './views/add-cover-photo/add-group-cover-photo/add-group-cover-photo.component';
 
 @NgModule({
   declarations: [
@@ -68,7 +69,8 @@ import { CreateGroupPostComponent } from './views/create-group-post/create-group
     VerifyEmailComponent,
     CheckEmailComponent,
     GroupHeaderComponent,
-    CreateGroupPostComponent
+    CreateGroupPostComponent,
+    //AddGroupCoverPhotoComponent,
   ],
   imports: [
     BrowserModule,
