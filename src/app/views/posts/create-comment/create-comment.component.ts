a27d051ae010e347b7d4479d418b0ae8f998fbd3
amsvang <amyvangsinbox@outlook.com>
2022-03-17 16:04:59 -0700

    fixed comment posting

--- src/app/views/posts/create-comment/create-comment.component.ts
+++ src/app/views/posts/create-comment/create-comment.component.ts
@@ -150,30 +150,36 @@ export class CreateCommentComponent implements OnInit {
       comment.writer = this.profile;
       this.commentService.createComment(comment).subscribe(
         (result)=>{
+          
+          console.log("lksdjf;lakj", result);
+           // send notification when comment is submitted
+          const fromProfileId = this.profile;
+          const toProfileId = this.post.creator;
+          const isRead = false;
+
+          this.commentNotification = {
+            fromProfileId: {
+              pid: fromProfileId.pid
+            },
+            toProfileId: {
+              pid: toProfileId.pid
+            },
+            postId: this.post,
+            cid: {
+              cid: result.cid
+            },
+            isRead: isRead
+          }
+          this.notificationService.postNotification(this.commentNotification).subscribe((data) => { 
+          });  
+
           this.isReply = false;
           this.comment.cbody = "";
           this.getOriginCommentsByPsid();
+
         }
       );
-      // send notification when comment is submitted
-      const fromProfileId = this.profile;
-      const toProfileId = this.post.creator;
-      const isRead = false;
-
-      this.commentNotification = {
-        fromProfileId: {
-          pid: fromProfileId.pid
-        },
-        toProfileId: {
-          pid: toProfileId.pid
-        },
-        postId: this.post,
-        cid: this.comment,
-        isRead: isRead
-      }
-
-      this.notificationService.postNotification(this.commentNotification).subscribe((data) => { 
-      });    
+      
     } // end if (this.post.body !== " ")
   } // end subumitComment(comment: Comment)
 
